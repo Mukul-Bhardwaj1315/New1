056a5c15cba77c2550b2ae821e883fa03c3265ce
Mukul-Bhardwaj1315 <bhardwajmukul1312@gmail.com>
2021-08-10 23:25:31 +0530

    First

---
+++ three.js
@@ -0,0 +1,22 @@
+const scene=new THREE.Scene()
+
+const geometry = new THREE.BoxGeometry(1,1,1)
+const material=new THREE.MeshBasicMaterial({color:0xff0000})
+const mesh=new THREE.Mesh(geometry,material)
+mesh.rotation.y=45
+scene.add(mesh)
+
+const sizes={width:800, height:600}
+const cam=new THREE.PerspectiveCamera(75, sizes.width/sizes.height)
+
+cam.position.z=2
+//cam.rotaion.x=15
+scene.add(cam)
+
+const canvas = document.querySelector('.webgl')
+const renderer = new THREE.WebGLRenderer({
+	canvas: canvas
+})
+renderer.setSize(sizes.width,sizes.height)
+
+renderer.render(scene, cam)
